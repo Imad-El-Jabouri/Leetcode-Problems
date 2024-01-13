5d9593c611756c808e3e505c7cecc7557b677c80
Imad <imad.el.github@github.com>
2024-01-13 14:33:58 +0100

    first commit

---
+++ Algorithms Problems/Easy/problem8.js
@@ -0,0 +1,28 @@
+// Problem On LeetCode : 771. Jewels and Stones
+
+
+// Solution
+
+
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+
+    let found = 0;
+
+    for (let i=0; i<jewels.length; i++) {
+        for (let j=0; j<stones.length; j++) {
+            if (jewels[i] === stones[j]) {
+                found++
+            }
+        }
+    }
+
+    return found
+};
+
+
+console.log(numJewelsInStones("aA", "aAAbbbb"))
